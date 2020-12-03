ade9f09489d7b83a37344779d60e2c26b8891d62
James Xu <36768789+jamesxu123@users.noreply.github.com>
2020-12-03 15:27:58 -0500

    Update DogShelterTest.java

--- src/test/java/assignment3/DogShelterTest.java
+++ src/test/java/assignment3/DogShelterTest.java
@@ -7,8 +7,71 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class DogShelterTest {
+
+    DogShelter.DogNode prev;
+
+    boolean isBST(DogShelter.DogNode node) {
+        prev = null;
+        if (node == null) {
+            return true;
+        }
+        return isBSTHelper(node) && isHeap(node) && validParentRef(node.younger) && validParentRef(node.older);
+    }
+
+    boolean isHeap(DogShelter.DogNode root) {
+        // base case
+        if (root == null) {
+            return true;
+        }
+
+        // current node has greater value than its left or right child
+        if ((root.younger != null && root.younger.d.getDaysAtTheShelter() >= root.d.getDaysAtTheShelter()) ||
+                (root.older != null && root.older.d.getDaysAtTheShelter() >= root.d.getDaysAtTheShelter())) {
+            return false;
+        }
+
+        // check for left and right subtree
+        return isHeap(root.younger) && isHeap(root.older);
+    }
+
+    boolean isBSTHelper(DogShelter.DogNode node) {
+        // traverse the tree in inorder fashion and
+        // keep a track of previous node
+        if (node != null) {
+            if (!isBSTHelper(node.younger))
+                return false;
+
+            // allows only distinct values node
+            if (prev != null && node.d.getAge() <= prev.d.getAge())
+                return false;
+            prev = node;
+            return isBSTHelper(node.older);
+        }
+        return true;
+    }
+
+    boolean validParentRef(DogShelter.DogNode childRoot) {
+        if (childRoot == null) {
+            return true;
+        }
+
+        if (childRoot.parent == null) {
+            return false;
+        }
+
+        boolean isChild = childRoot.parent.younger == childRoot || childRoot.parent.older == childRoot;
+
+        if (!isChild) {
+            return false;
+        }
+
+        return validParentRef(childRoot.younger) && validParentRef(childRoot.older);
+    }
 
     DogShelter generateSampleShelter() {
         Dog R = new Dog("Rex", 8, 100, 5, 50.0);
@@ -24,6 +87,26 @@ public class DogShelterTest {
         shelter.shelter(S);
         shelter.shelter(L);
         shelter.shelter(Z);
+        return shelter;
+    }
+
+    DogShelter generateLargeSampleShelter() {
+        Dog R = new Dog("Rex", 8, 100, 5, 50.0);
+        Dog S = new Dog("Stella", 5, 50, 2, 250.0);
+        Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
+        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Dog B = new Dog("Bella", 1, 55, 15, 120.0);
+        Dog C = new Dog("Cloud", 4, 10, 23, 80.0);
+        Dog A = new Dog("Archie", 6, 120, 18, 40.0);
+        Dog D = new Dog("Daisy", 7, 15, 12, 35.0);
+        DogShelter shelter = new DogShelter(R);
+        shelter.shelter(S);
+        shelter.shelter(L);
+        shelter.shelter(P);
+        shelter.shelter(B);
+        shelter.shelter(C);
+        shelter.shelter(A);
+        shelter.shelter(D);
         return shelter;
     }
 
@@ -71,6 +154,16 @@ public class DogShelterTest {
         DogShelter.DogNode STest = (DogShelter.DogNode) LNode.get(RTest);
         Dog s = (Dog) dog.get(STest);
         Assertions.assertEquals(S.toString(), s.toString());
+        Assertions.assertTrue(isBST(shelter.root));
+    }
+
+    @Test
+    @DisplayName("Should test generateSampleShelter and generateLargeSampleShelter")
+    void testShelter2() {
+        Assertions.assertAll(
+                () -> isBST(generateSampleShelter().root),
+                () -> isBST(generateLargeSampleShelter().root)
+        );
     }
 
     @Test
@@ -83,12 +176,49 @@ public class DogShelterTest {
     }
 
     @Test
+    @DisplayName("Should find the oldest dog in a sub tree")
+    void testFindOldestSub() {
+        DogShelter shelter = generateLargeSampleShelter();
+        Dog d = shelter.root.older.findOldest();
+        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Assertions.assertEquals(P,d);
+    }
+
+    @Test
+    @DisplayName("Should return null for findOldest in empty tree")
+    void testFindOldestNull() {
+        DogShelter shelter = new DogShelter(new Dog("asd", 1, 2, 3,4));
+        shelter.adopt();
+        Dog d = shelter.findOldest();
+//        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Assertions.assertNull(d);
+    }
+
+    @Test
     @DisplayName("Should find the youngest dog in a shelter")
     void testFindYoungest() {
         DogShelter shelter = generateSampleShelter();
         Dog d = shelter.findYoungest();
         Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
         Assertions.assertEquals(d.toString(), L.toString());
+    }
+    @Test
+    @DisplayName("Should find the youngest dog in a sub tree")
+    void testFindYoungestSub() {
+        DogShelter shelter = generateLargeSampleShelter();
+        Dog d = shelter.root.older.findYoungest();
+        Dog D = new Dog("Daisy", 7, 15, 12, 35.0);
+        Assertions.assertEquals(D, d);
+    }
+
+    @Test
+    @DisplayName("Should return null for findYoungest in empty tree")
+    void testFindYoungestNull() {
+        DogShelter shelter = new DogShelter(new Dog("asd", 1, 2, 3,4));
+        shelter.adopt();
+        Dog d = shelter.findYoungest();
+//        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Assertions.assertNull(d);
     }
 
     @Test
@@ -109,7 +239,53 @@ public class DogShelterTest {
         expected.add(S);
         expected.add(R);
 
-        Assertions.assertTrue(expected.containsAll(dogs));
+        Assertions.assertEquals(dogs, expected);
+    }
+
+    @Test
+    @DisplayName("Should throw exception for DogNode Iterator on no next")
+    void testIterator2() {
+        DogShelter shelter = generateSampleShelter();
+        for (int i = 0; i < 4; i++) {
+            shelter.adopt();
+        }
+        try {
+            Iterator<Dog> iterator = shelter.iterator();
+            iterator.next();
+            Assertions.fail("Did not throw exception");
+        } catch (NoSuchElementException e) {
+            System.out.println("Successfully thrown exception");
+        }
+    }
+
+    @Test
+    @DisplayName("Should test iterator on large sample shelter")
+    void testIterator3() {
+        DogShelter largeShelter = generateLargeSampleShelter();
+        Dog R = new Dog("Rex", 8, 100, 5, 50.0);
+        Dog S = new Dog("Stella", 5, 50, 2, 250.0);
+        Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
+        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Dog B = new Dog("Bella", 1, 55, 15, 120.0);
+        Dog C = new Dog("Cloud", 4, 10, 23, 80.0);
+        Dog A = new Dog("Archie", 6, 120, 18, 40.0);
+        Dog D = new Dog("Daisy", 7, 15, 12, 35.0);
+        ArrayList<Dog> expected = new ArrayList<>();
+        expected.add(R);
+        expected.add(S);
+        expected.add(L);
+        expected.add(P);
+        expected.add(B);
+        expected.add(C);
+        expected.add(A);
+        expected.add(D);
+        expected.sort(Comparator.comparingInt(Dog::getAge));
+
+        ArrayList<Dog> result = new ArrayList<>();
+        for (Dog d : largeShelter) {
+            result.add(d);
+        }
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
@@ -118,6 +294,15 @@ public class DogShelterTest {
         DogShelter shelter = generateSampleShelter();
         double budget = shelter.budgetVetExpenses(9);
         Assertions.assertEquals(budget, 325);
+    }
+
+    @Test
+    @DisplayName("Should find correct vet budget for null tree")
+    void testVetBudget2() {
+        DogShelter shelter = new DogShelter(new Dog("asd", 1, 2, 3,4));
+        shelter.adopt();
+        double budget = shelter.budgetVetExpenses(9);
+        Assertions.assertEquals(0, budget);
     }
 
     @Test
@@ -157,6 +342,7 @@ public class DogShelterTest {
         DogShelter shelter = generateSampleShelter();
         Dog Z = new Dog("Z", 4, 200, 10, 25.0);
         Assertions.assertEquals(shelter.adopt(), Z);
+        Assertions.assertTrue(isBST(shelter.root));
     }
 
     @Test
@@ -168,9 +354,13 @@ public class DogShelterTest {
         Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
         Dog Z = new Dog("Z", 4, 200, 10, 25.0);
         Assertions.assertEquals(shelter.adopt(), Z);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), R);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), L);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), S);
+        Assertions.assertTrue(isBST(shelter.root));
     }
 
     @Test
@@ -184,8 +374,12 @@ public class DogShelterTest {
         shelter.shelter(L);
 
         Assertions.assertEquals(shelter.adopt(), R);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), S);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), L);
+        Assertions.assertTrue(isBST(shelter.root));
+        Assertions.assertNull(shelter.root);
     }
 
     @Test
@@ -199,8 +393,219 @@ public class DogShelterTest {
         shelter.shelter(L);
 
         Assertions.assertEquals(shelter.adopt(), R);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), S);
+        Assertions.assertTrue(isBST(shelter.root));
         Assertions.assertEquals(shelter.adopt(), L);
+        Assertions.assertTrue(isBST(shelter.root));
+        Assertions.assertNull(shelter.root);
+    }
+
+    @Test
+    @DisplayName("Should test adopt from bottom with all-right branches")
+    void testAdopt5() {
+        Dog R = new Dog("Rex", 8, 100, 5, 50.0);
+        Dog S = new Dog("Stella", 10, 90, 2, 250.0);
+        Dog L = new Dog("Lessie", 12, 80, 9, 25.0);
+        DogShelter shelter = new DogShelter(R);
+        shelter.shelter(S);
+        shelter.shelter(L);
+
+        shelter.adopt(L);
+        Assertions.assertEquals(shelter.root.d, R);
+        Assertions.assertEquals(shelter.root.older.d, S);
+        Assertions.assertNull(shelter.root.younger);
+        Assertions.assertEquals(shelter.root.older.parent, shelter.root);
+        Assertions.assertTrue(isBST(shelter.root));
+
+        shelter.adopt(S);
+        Assertions.assertNull(shelter.root.older);
+        Assertions.assertNull(shelter.root.younger);
+        Assertions.assertTrue(isBST(shelter.root));
+
+        shelter.adopt(R);
+        Assertions.assertNull(shelter.root);
+    }
+
+    @Test
+    @DisplayName("Should comprehensively test adopt")
+    void testAdopt6() {
+        DogShelter largeShelter = generateLargeSampleShelter();
+        Dog R = new Dog("Rex", 8, 100, 5, 50.0);
+        Dog S = new Dog("Stella", 5, 50, 2, 250.0);
+        Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
+        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Dog B = new Dog("Bella", 1, 55, 15, 120.0);
+        Dog C = new Dog("Cloud", 4, 10, 23, 80.0);
+        Dog A = new Dog("Archie", 6, 120, 18, 40.0);
+        Dog D = new Dog("Daisy", 7, 15, 12, 35.0);
+
+        largeShelter.adopt(A);
+        Assertions.assertEquals(R, largeShelter.root.d);
+        Assertions.assertEquals(P, largeShelter.root.older.d);
+        Assertions.assertTrue(isBST(largeShelter.root));
+
+        largeShelter.adopt(S);
+        Assertions.assertEquals(D, largeShelter.root.younger.older.d);
+        Assertions.assertEquals(C, largeShelter.root.younger.older.younger.d);
+//        System.out.println("hi");
+        Assertions.assertTrue(isBST(largeShelter.root));
+
+
+        largeShelter.adopt(B);
+        Assertions.assertNull(largeShelter.root.younger.younger);
+        Assertions.assertTrue(isBST(largeShelter.root));
+
+
+        largeShelter.adopt(C);
+        Assertions.assertNull(largeShelter.root.younger.older.younger);
+        Assertions.assertTrue(isBST(largeShelter.root));
+
+        largeShelter.adopt(L);
+        Assertions.assertEquals(D, largeShelter.root.younger.d);
+        Assertions.assertTrue(isBST(largeShelter.root));
+
+    }
+
+    @Test
+    @DisplayName("Should comprehensively test adopt #2")
+    void testAdopt7() {
+        DogShelter largeShelter = generateLargeSampleShelter();
+        Dog R = new Dog("Rex", 8, 100, 5, 50.0);
+        Dog S = new Dog("Stella", 5, 50, 2, 250.0);
+        Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
+        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Dog B = new Dog("Bella", 1, 55, 15, 120.0);
+        Dog C = new Dog("Cloud", 4, 10, 23, 80.0);
+        Dog A = new Dog("Archie", 6, 120, 18, 40.0);
+        Dog D = new Dog("Daisy", 7, 15, 12, 35.0);
+
+        largeShelter.adopt(R);
+        Assertions.assertEquals(P, largeShelter.root.older.d);
+        Assertions.assertTrue(isBST(largeShelter.root));
+
+
+        largeShelter.adopt(B);
+        Assertions.assertNull(largeShelter.root.younger.younger);
+        Assertions.assertTrue(isBST(largeShelter.root));
+    }
+
+    @Test
+    @DisplayName("Should test adopting on non-root node")
+    void testAdopt8() {
+        Dog R = new Dog("Rex", 8, 100, 5, 50.0);
+        Dog S = new Dog("Stella", 5, 50, 2, 250.0);
+        Dog L = new Dog("Lessie", 3, 70, 9, 25.0);
+        Dog P = new Dog("Poldo", 10, 60, 1, 35.0);
+        Dog B = new Dog("Bella", 1, 55, 15, 120.0);
+        Dog C = new Dog("Cloud", 4, 10, 23, 80.0);
+        Dog A = new Dog("Archie", 6, 120, 18, 40.0);
+        Dog D = new Dog("Daisy", 7, 15, 12, 35.0);
+
+        DogShelter s = new DogShelter(R);
+        s.shelter(S);
+        s.shelter(L);
+        s.shelter(P);
+        s.shelter(B);
+        s.shelter(C);
+        s.shelter(A);
+        s.shelter(D);
+
+//        System.out.println("Before: ");
+//        System.out.println(s.root);
+        String val0 = s.root.toString();
+        String expected0 = "Archie(6 , 120)\n" +
+                "younger than Archie(6 , 120) :\n" +
+                "Lessie(3 , 70)\n" +
+                "younger than Lessie(3 , 70) :\n" +
+                "Bella(1 , 55)\n" +
+                "older than Lessie(3 , 70) :\n" +
+                "Stella(5 , 50)\n" +
+                "younger than Stella(5 , 50) :\n" +
+                "Cloud(4 , 10)\n" +
+                "older than Archie(6 , 120) :\n" +
+                "Rex(8 , 100)\n" +
+                "younger than Rex(8 , 100) :\n" +
+                "Daisy(7 , 15)\n" +
+                "older than Rex(8 , 100) :\n" +
+                "Poldo(10 , 60)\n";
+        Assertions.assertEquals(expected0, val0);
+        Assertions.assertTrue(isBST(s.root));
+/* Displays:
+Before:
+Archie(6 , 120)
+younger than Archie(6 , 120) :
+Lessie(3 , 70)
+younger than Lessie(3 , 70) :
+Bella(1 , 55)
+older than Lessie(3 , 70) :
+Stella(5 , 50)
+younger than Stella(5 , 50) :
+Cloud(4 , 10)
+older than Archie(6 , 120) :
+Rex(8 , 100)
+younger than Rex(8 , 100) :
+Daisy(7 , 15)
+older than Rex(8 , 100) :
+Poldo(10 , 60)
+*/
+
+
+        DogShelter.DogNode x = s.root.younger.adopt(L);
+        Assertions.assertTrue(isBST(s.root));
+
+//        System.out.println("\nAfter the adoption: ");
+//        System.out.println(x);
+        String val = x.toString();
+        String expected = "Bella(1 , 55)\n"
+                + "older than Bella(1 , 55) :\n"
+                + "Stella(5 , 50)\n"
+                + "younger than Stella(5 , 50) :\n"
+                + "Cloud(4 , 10)\n";
+        Assertions.assertEquals(expected, val);
+/* Displays
+After the adoption:
+Bella(1 , 55)
+older than Bella(1 , 55) :
+Stella(5 , 50)
+younger than Stella(5 , 50) :
+Cloud(4 , 10)
+*/
+//
+//        System.out.println("\nAfter the adoption: ");
+//        System.out.println(s.root);
+        String val2 = s.root.toString();
+        String expected2 = "Archie(6 , 120)\n"
+                + "younger than Archie(6 , 120) :\n"
+                + "Bella(1 , 55)\n"
+                + "older than Bella(1 , 55) :\n"
+                + "Stella(5 , 50)\n"
+                + "younger than Stella(5 , 50) :\n"
+                + "Cloud(4 , 10)\n"
+                + "older than Archie(6 , 120) :\n"
+                + "Rex(8 , 100)\n"
+                + "younger than Rex(8 , 100) :\n"
+                + "Daisy(7 , 15)\n"
+                + "older than Rex(8 , 100) :\n"
+                + "Poldo(10 , 60)\n";
+        Assertions.assertEquals(expected2, val2);
+        Assertions.assertTrue(isBST(s.root));
+/* Displays
+After the adoption:
+Archie(6 , 120)
+younger than Archie(6 , 120) :
+Bella(1 , 55)
+older than Bella(1 , 55) :
+Stella(5 , 50)
+younger than Stella(5 , 50) :
+Cloud(4 , 10)
+older than Archie(6 , 120) :
+Rex(8 , 100)
+younger than Rex(8 , 100) :
+Daisy(7 , 15)
+older than Rex(8 , 100) :
+Poldo(10 , 60)
+*/
     }
 
 
@@ -222,12 +627,14 @@ public class DogShelterTest {
         shelter.adopt(R);
 
         Assertions.assertEquals(shelter.root.d, L);
+        Assertions.assertTrue(isBST(shelter.root));
 
         shelter.shelter(R);
         shelter.shelter(P);
         shelter.adopt(R);
 
         Assertions.assertEquals(shelter.root.d, L);
+        Assertions.assertTrue(isBST(shelter.root));
 
         shelter.shelter(R);
         shelter.shelter(B);
@@ -253,7 +660,9 @@ public class DogShelterTest {
                             () -> Assertions.assertEquals(schedule.get(2).get(1), A),
                             () -> Assertions.assertEquals(schedule.get(3).get(0), C)
                     );
-                }
+                },
+                () -> Assertions.assertTrue(isBST(shelter.root))
+
         );
 
         Assertions.assertEquals(shelter.adopt(), A);
@@ -270,16 +679,32 @@ public class DogShelterTest {
                 () -> Assertions.assertEquals(root.younger.older.younger.d, C),
                 () -> Assertions.assertEquals(root.younger.older.older.d, D),
                 () -> Assertions.assertEquals(root.younger.older.older.parent, root.younger.older),
-                () -> Assertions.assertEquals(root.younger.older.younger.parent, root.younger.older)
+                () -> Assertions.assertEquals(root.younger.older.younger.parent, root.younger.older),
+                () -> Assertions.assertTrue(isBST(shelter.root))
         );
 
         Assertions.assertEquals(shelter.adopt(), R);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertEquals(shelter.adopt(), L);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertEquals(shelter.adopt(), P);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertEquals(shelter.adopt(), B);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertEquals(shelter.adopt(), S);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertEquals(shelter.adopt(), D);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertEquals(shelter.adopt(), C);
+        Assertions.assertTrue(isBST(shelter.root));
+
         Assertions.assertNull(shelter.root);
+        Assertions.assertTrue(isBST(shelter.root));
     }
 }
